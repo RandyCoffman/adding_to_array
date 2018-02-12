629b814a8a3667a9b4d48bcd6327eaef66015d7c
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-12 13:43:09 -0500

    I added comments detailing what each line does

--- array_2.rb
+++ array_2.rb
@@ -1,7 +1,7 @@
-fruit = []
+fruit = []															# Creates an empty array named fruit
 
-fruit.push("apple", "banana", "orange", "pear")
+fruit.push("apple", "banana", "orange", "pear")						# Pushes the strings in order to the end of the array.
 
 fruit.each do |fruit|
-	puts "This is a type of fruit: #{fruit}"
+	puts "This is a type of fruit: #{fruit}"						# This prints the string along with each individal string in the array 
 end
