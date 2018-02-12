008bbb1a1ac9175f5aed24d6e6e8f512125b0e20
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-12 13:56:09 -0500

    I added comments detailing what each line does

--- basket.rb
+++ basket.rb
@@ -1,14 +1,14 @@
-fruit = []
+fruit = []															# creates an empty array named fruit 
 
-vegetables = []
+vegetables = []														# creates an empty array named fruit
 
-fruit.push("apple", "banana", "orange", "pear")
+fruit.push("apple", "banana", "orange", "pear")						# this pushes the strings into the fruit array
 
-vegetables.push("potato", "carrot", "lettuce", "spinach")
+vegetables.push("potato", "carrot", "lettuce", "spinach")			# This pushes the strings into the vegetable array
 
-fruit.shift
-vegetables.pop
+fruit.shift															# This removes the first string in the fruit array named "apple"
+vegetables.pop														# This removes the last string in the vegetables array named "spinach"
 
-basket = fruit.concat vegetables
+basket = fruit.concat vegetables									# This combines fruit and vegetables arrays into one array named basket
 
-print basket
+print basket														# prints the contents of the array "basket"
