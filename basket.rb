adf99d339f1fda1b35780aa6414ab4ccb817e065
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-12 13:42:24 -0500

    first commit

---
+++ basket.rb
@@ -0,0 +1,14 @@
+fruit = []
+
+vegetables = []
+
+fruit.push("apple", "banana", "orange", "pear")
+
+vegetables.push("potato", "carrot", "lettuce", "spinach")
+
+fruit.shift
+vegetables.pop
+
+basket = fruit.concat vegetables
+
+print basket
